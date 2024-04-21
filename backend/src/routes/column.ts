e6e7c43280df896932cdc3a938230ea758d6e40f
mynk2611 <mehta26mayank@gmail.com>
2024-04-21 21:12:50 +0530

    add authorization check

--- backend/src/routes/column.ts
+++ backend/src/routes/column.ts
@@ -1,11 +1,45 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { verify } from "hono/jwt";
+
 
 const column = new Hono<{
     Bindings: {
         DATABASE_URL : string,
-    }}>();
+        JWT_SECRET : string,
+    },
+    Variables : {
+        userId : string,
+    }
+}>();
+
+column.use('/*', async (c, next)=>{
+    const header = c.req.header("authorization") || "";
+
+   try{
+        const decode = await verify(header, c.env.JWT_SECRET);
+
+            if(decode){
+                c.set("userId", decode.id);
+                await next()
+            }else{
+                c.status(403)
+                return c.json({
+                    message : "You are not logged in"
+                })
+            }
+   }
+   catch(error){
+        console.log("error during authentication", error);
+
+        c.status(500)
+        return c.json({
+            message :  "Internal server error",
+        });
+   }
+    
+})  
 
 column.post('/', async (c) => {
     const prisma = new PrismaClient({
