fc1e350ad0a4b5aadc028c9f3811ba104abfe8e0
mynk2611 <mehta26mayank@gmail.com>
2024-04-24 04:51:07 +0530

    fix outline

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -5,7 +5,7 @@ interface buttonInput {
 export const Button = ({title}: buttonInput)=>{
     return(
     <div className="w-full mt-4">
-        <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-xl px-5 py-2.5 me-2 mb-2 w-full">
+        <button type="button" className="outline-none  text-white bg-green-700 hover:bg-green-800  font-medium rounded-lg text-xl px-5 py-2.5 me-2 mb-2 w-full">
                 {title}
         </button>
 
