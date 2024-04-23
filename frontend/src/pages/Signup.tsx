e9954b01b50af5a99c18852b8bb52f65a38c0ba2
mynk2611 <mehta26mayank@gmail.com>
2024-04-24 04:19:28 +0530

    add signup page

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,15 @@
+import { AuthSignup } from "../components/AuthSignup"
+import { Quote } from "../components/Quote"
+
 export const Signup = ()=>{
     return(
-    <div>
-        Signup
+    <div className="grid grid-cols-2">
+        <div>
+            <Quote/>
+        </div>
+        <div>
+            <AuthSignup/>
+        </div>
     </div>
     )
 
