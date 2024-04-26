bad0111bbc270ca35faf0d08133f2daaadf0d009
mynk2611 <mehta26mayank@gmail.com>
2024-04-26 18:48:58 +0530

    fix error handler

--- backend/src/routes/column.ts
+++ backend/src/routes/column.ts
@@ -52,150 +52,159 @@ column.use('/*', async(c, next)=>{
     await next();
 })
 
-column.post('/', async (c) => {
-    const body = await c.req.json();
-    const {success} = columnSchema.safeParse(body)
-
-        if(!success){
-            c.status(400);
-            return c.json({
-                message : "Wrong inputs",
-            })
-        }
-    const prisma = c.get('prisma')
-    const authorId = c.get('userId')
-
-
-    try{
-        const column = await prisma.column.create({
-            data : {
-                title : body.title,
-                content : body.content,
-                published : true,
-                author_id : authorId,
-            }
-        })
-
-        return c.json({
-            id : column.id,
-        })
-
-    }
-    catch(error){
-        console.log("Error while creating column", error);
-        c.status(500)
-        return c.json({
-            message : "Error while creating column"
-        })        
-    }
-})
-
-column.put('/', async (c) => {
-    const body = await c.req.json();
-    const {success} = updateColumnSchema.safeParse(body);
-         
-        if(!success){
-            c.status(400);
-            return c.json({
-                message : "Wrong inputs",
-            })
-        }
-    const prisma = c.get('prisma')
-
-
-    try {
-        const column = await prisma.column.update({
-            where : {
-                id : body.id
-            },
-            data : {
-                title : body.title,
-                content : body.content,
-            }
-        })
-
-        return c.json({
-            id : column.id,
-        })
-    }
-    catch(error){
-        console.log("Error while updating" , error);
-        c.status(500)
-        return c.json({
-            message : "Internal server error"
-        })
-    }
-})
-
-column.post('/bulk', async (c) => {
-    const prisma = c.get('prisma')
-    const authorId = c.get('userId')
-
-    try{
-        const columns = await prisma.column.findMany({
-            select : {
-                id : true,
-                title : true,
-                content : true,
-                user : {
-                    select : {
-                        name : true
-                    }
-                }
-            }
-        })
-
-        return c.json({
-            columns
-        })
-    }
-    catch(error){
-        console.log("Error while fetching the data", error);
-        c.status(411)
-        return c.json({
-            message : "Internal server error"
-        })
-    }
-})
-
-// column.post('/:author-name', async (c) => {
-//     const prisma = c.get('prisma')
-// })
-
-column.post('/:id', async (c) => {
-    const prisma = c.get('prisma')
-    const authorId = c.get('userId');
-
-    const targetId = c.req.param("id");
-
-    try{
-        const column = await prisma.column.findFirst({
-            where : {
-                id : targetId,
-            },
-            select : {
-                id : true,
-                title : true,
-                content : true,
-                user : {
-                    select : {
-                        name : true,
-                    }
-                }
-            }
-        })
+    column.post('/', async (c) => {
+        const body = await c.req.json();
         
-        return c.json({
-            column,
-        })
-    }
-    catch(error){
-        console.log("Error while fetching data", error);
-        c.status(411)
-        return c.json({
-            message : "Internal server error"
-        })
-    }
-})
+        const prisma = c.get('prisma')
+        const authorId = c.get('userId')
+
+
+        try{
+            
+            const parsedData = columnSchema.safeParse(body);
+
+            if (!parsedData.success) {
+                const error = parsedData.error;
+                c.status(400);
+                return c.json({
+                    message: "Validation Error",
+                    errors: error.errors.map(err => err.message)
+                });
+            }
+
+            const column = await prisma.column.create({
+                data : {
+                    title : body.title,
+                    content : body.content,
+                    published : true,
+                    author_id : authorId,
+                }
+            })
+
+            return c.json({
+                id : column.id,
+            })
+
+        }
+        catch(error){
+            console.log("Error while creating column", error);
+            c.status(500)
+            return c.json({
+                message : "Error while creating column"
+            })        
+        }
+    })
+
+    column.put('/', async (c) => {
+        const body = await c.req.json();
+        const prisma = c.get('prisma')
+
+
+        try {
+
+            const parsedData = updateColumnSchema.safeParse(body);
+
+            if (!parsedData.success) {
+                const error = parsedData.error;
+                c.status(400);
+                return c.json({
+                    message: "Validation Error",
+                    errors: error.errors.map(err => err.message)
+                });
+            }
+
+            const column = await prisma.column.update({
+                where : {
+                    id : body.id
+                },
+                data : {
+                    title : body.title,
+                    content : body.content,
+                }
+            })
+
+            return c.json({
+                id : column.id,
+            })
+        }
+        catch(error){
+            console.log("Error while updating" , error);
+            c.status(500)
+            return c.json({
+                message : "Internal server error"
+            })
+        }
+    })
+
+    column.get('/bulk', async (c) => {
+        const prisma = c.get('prisma')
+        const authorId = c.get('userId')
+
+        try{
+            const columns = await prisma.column.findMany({
+                select : {
+                    id : true,
+                    title : true,
+                    content : true,
+                    user : {
+                        select : {
+                            name : true
+                        }
+                    }
+                }
+            })
+
+            return c.json({
+                columns
+            })
+        }
+        catch(error){
+            console.log("Error while fetching the data", error);
+            c.status(411)
+            return c.json({
+                message : "Internal server error"
+            })
+        }
+    })
+
+    // column.post('/:author-name', async (c) => {
+    //     const prisma = c.get('prisma')
+    // })
+
+    column.post('/:id', async (c) => {
+        const prisma = c.get('prisma')
+        const authorId = c.get('userId');
+
+        const targetId = c.req.param("id");
+
+        try{
+            const column = await prisma.column.findFirst({
+                where : {
+                    id : targetId,
+                },
+                select : {
+                    id : true,
+                    title : true,
+                    content : true,
+                    user : {
+                        select : {
+                            name : true,
+                        }
+                    }
+                }
+            })
+            
+            return c.json({
+                column,
+            })
+        }
+        catch(error){
+            console.log("Error while fetching data", error);
+            c.status(411)
+            return c.json({
+                message : "Internal server error"
+            })
+        }
+    })
 
 export default column;
