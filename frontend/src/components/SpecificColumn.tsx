f0902f5806df0b540fd5d1bd08ede3ac76b9f36c
mynk2611 <mehta26mayank@gmail.com>
2024-04-29 21:05:12 +0530

    update author div

--- frontend/src/components/SpecificColumn.tsx
+++ frontend/src/components/SpecificColumn.tsx
@@ -6,7 +6,7 @@ export const SpecificColumn = ({ column }: { column: uniCol }) => {
         <div>
             <AppBar/>
             <div className="flex justify-center">
-                <div className="grid grid-cols-12 px-10 mt-8 max-w-screen-xl">
+                <div className="grid grid-cols-12 px-5 mt-9 max-w-screen-xl">
                         <div className="col-span-8 ">
                                 <div className="text-5xl font-black " >
                                     {column.title}
@@ -21,8 +21,27 @@ export const SpecificColumn = ({ column }: { column: uniCol }) => {
                                 </div>
                         </div>
 
-                        <div className="col-span-4 pl-7 pt-3 ">
-                            hi there
+                        <div className="col-span-4 pl-10 pt-3 ">
+                            <div >
+                                Author
+                            </div>
+
+                            <div className="flex justify-normal mt-3">
+                                <div className="flex flex-col justify-center">
+                                    <div className="w-6 h-6 rounded-full bg-gray-200">
+                                            {/* pointer */}
+                                    </div>
+                                </div>
+
+                                <div className="ml-4">
+                                        <div className="text-xl font-bold">
+                                            {column.user.name}
+                                        </div>
+                                        <div className="text-sm font-medium text-gray-600">
+                                            {column.user.bio}
+                                        </div>
+                                </div>
+                           </div>
                         </div>
                 </div>
             </div>
